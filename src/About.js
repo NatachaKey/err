9cce45af2ff9432d815db288a0a72cf3351c249b
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-11-22 16:19:43 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,23 +1,18 @@
 import video from './VideoOne.mp4';
 const About = ()=>{
     return(
-    
-            <div >
-                <div className='aboutLines'>
+         <div >
+            <video autoPlay muted loop className='background-video'>
+                <source src={video} type="video/mp4" />                
+            </video>
+
+            <div className='aboutLines'>
                <p className='par'>CANDYSHOP</p>
                <p className='par'>Your favourite genuine sweets</p>
-            <p className='par italic'>Since 1948</p>  
+                <p className='par italic'>Since 1948</p>  
             </div>
-            <video autoPlay muted loop className='background-video'>
-                <source src={video} type="video/mp4" />
-                 </video>    
            
-            
-       
-        </div>
-       
-      
-    
+        </div>   
     )
 }
 
