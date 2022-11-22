98ba21b47c4d1a3ac7e945ef20986db9303d194c
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-11-22 16:10:30 +0100

    Update AllCategories.js

--- src/Components/Filter/AllCategories.js
+++ src/Components/Filter/AllCategories.js
@@ -4,7 +4,7 @@ const AllCategories = ()=> {
     return(<div>
         <h1>Smth sweety this way comes</h1>
         
-        { ['ALL','CAKE', 'COOKIES', 'MUFFINS'].map((category, index)=> <Filter key={index} category={category}/>)}
+        { ['ALL','COOKIES', 'CAKE', 'MUFFINS'].map((category, index)=> <Filter key={index} category={category}/>)}
         
     </div>)
 }
